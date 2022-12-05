35a7fb70b7cfbe2bc97db8e9d76746d53f47bc3b
arkaroy135 <arka.roy@live.com>
2022-12-05 11:22:48 +0530

    logic added

--- Ping-Coming-Soon-Page/script.js
+++ Ping-Coming-Soon-Page/script.js
@@ -3,16 +3,16 @@ const msz = document.querySelector(".error_messege");
 const btn = document.querySelector(".notify");
 
 btn.addEventListener("click", (e) => {
-  e.preventDefault();
   if (email.value === "") {
     msz.innerText = "Whoops! It looks like you forgot to add your email";
     email.style.borderColor = "hsl(354, 100%, 66%)";
+    e.preventDefault();
   } else if (!validateEmail(email.value)) {
     msz.innerText = "Please provide a valid email address";
     email.style.borderColor = "hsl(354, 100%, 66%)";
+    e.preventDefault();
   } else {
-    msz.innerText = " ";
-    email.style.borderColor = "green";
+    alert("Thank You. You will Get Email Updates.");
   }
 });
 

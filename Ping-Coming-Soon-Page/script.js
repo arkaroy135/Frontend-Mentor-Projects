e889fa94333c57cca8da8410515c0535010f8454
arkaroy135 <arka.roy@live.com>
2022-12-05 09:17:07 +0530

    Completed without any visible bugs

---
+++ Ping-Coming-Soon-Page/script.js
@@ -0,0 +1,22 @@
+const email = document.querySelector("#email-id");
+const msz = document.querySelector(".error_messege");
+const btn = document.querySelector(".notify");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (email.value === "") {
+    msz.innerText = "Whoops! It looks like you forgot to add your email";
+    email.style.borderColor = "hsl(354, 100%, 66%)";
+  } else if (!validateEmail(email.value)) {
+    msz.innerText = "Please provide a valid email address";
+    email.style.borderColor = "hsl(354, 100%, 66%)";
+  } else {
+    msz.innerText = " ";
+    email.style.borderColor = "green";
+  }
+});
+
+function validateEmail() {
+  let exp = /^[A-Za-z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,}$/;
+  return exp.test(String(email.value).toLowerCase());
+}

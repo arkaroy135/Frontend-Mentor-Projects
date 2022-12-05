697a9bb399246c0527d00d14f57332457967b675
arkaroy135 <arka.roy@live.com>
2022-12-05 23:46:50 +0530

    logic added

--- Base-Apparel-Coming-Soon/script.js
+++ Base-Apparel-Coming-Soon/script.js
@@ -1,3 +1,25 @@
+const email = document.querySelector("#user_email");
+const form = document.querySelector("#form");
+const msz = document.querySelector(".error-messege");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (email.value === "") {
+    msz.style.color = "hsl(0, 93%, 68%)";
+    msz.innerText = "Woops! You forgot to provide any email-id";
+    email.style.borderColor = "hsl(0, 93%, 68%)";
+  } else if (!validateEmail(email.value)) {
+    msz.style.color = "hsl(0, 93%, 68%)";
+    msz.innerText = "Please provide a valid email-id";
+    email.style.borderColor = "hsl(0, 93%, 68%)";
+  } else {
+    msz.style.color = "green";
+    msz.innerText = "Thank You. Your email has been registered with us.";
+    email.value = "";
+    email.style.borderColor = "";
+  }
+});
+
 function validateEmail() {
   let exp = /^[A-Za-z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,}$/;
   return exp.test(String(email.value).toLowerCase());

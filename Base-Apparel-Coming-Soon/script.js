1be23f1fbd8bae4af0320574a227a0b348dec077
arkaroy135 <arka.roy@live.com>
2022-12-06 11:24:47 +0530

    Both mobile and big screen view completed.

--- Base-Apparel-Coming-Soon/script.js
+++ Base-Apparel-Coming-Soon/script.js
@@ -1,6 +1,7 @@
 const email = document.querySelector("#user_email");
 const form = document.querySelector("#form");
 const msz = document.querySelector(".error-messege");
+const error = document.querySelector(".error");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,15 +9,19 @@ form.addEventListener("submit", (e) => {
     msz.style.color = "hsl(0, 93%, 68%)";
     msz.innerText = "Woops! You forgot to provide any email-id";
     email.style.borderColor = "hsl(0, 93%, 68%)";
+    // error.classList.remove("hidden");
+    error.style.display = "block";
   } else if (!validateEmail(email.value)) {
     msz.style.color = "hsl(0, 93%, 68%)";
     msz.innerText = "Please provide a valid email-id";
     email.style.borderColor = "hsl(0, 93%, 68%)";
+    error.style.display = "block";
   } else {
     msz.style.color = "green";
     msz.innerText = "Thank You. Your email has been registered with us.";
     email.value = "";
     email.style.borderColor = "";
+    error.style.display = "none";
   }
 });
 
